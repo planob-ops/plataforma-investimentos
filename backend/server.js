f2b7a7950a44b16daf2bcffab1beb17f15b7e7d3
planob-ops <manokamela@gmail.com>
2025-05-06 16:31:41 +0100

    Adicionar arquivo server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-  ├── server.js            -> Arquivo principal do servidor
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middleware para processamento de JSON
+app.use(express.json());
+
+// Rota padrão para teste
+app.get('/', (req, res) => {
+  res.send('Bem-vindo ao backend da plataforma de investimentos!');
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
